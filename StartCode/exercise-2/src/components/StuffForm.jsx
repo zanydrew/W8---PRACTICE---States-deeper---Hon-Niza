cf96cd0e3be3b9414ce07e52be8bf7e206e896be
zanydrew <nizahon00@gmail.com>
2026-03-12 09:14:58 +0700

    gitignore

---
+++ StartCode/exercise-2/src/components/StuffForm.jsx
@@ -0,0 +1,13 @@
+export default function StuffForm() {
+  return (
+    <form className="stuff-form">
+      <p>Stuff name</p>
+      <input type="search" placeholder="Banana" />
+
+      <p>Stuff price</p>
+      <input type="search" placeholder="15" />
+
+      <button>Add Stuff</button>
+    </form>
+  );
+}
